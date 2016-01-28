c25fd21a233083dd6e4e72a9a672afdf541750d7
nonsenz <s.kranz@gmail.com>
2016-01-28 22:24:06 +0100

    next track starts after track finishes. made yt-player fullscreen even on resize :-P

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
 var bbcParty = (function main() {
-    var hit = "";
+        var hit = "",
+            getNextHit = function() {
+                nanoajax.ajax({url:'/hit'}, function (code, responseText) {
+                    hit = JSON.parse(responseText).id;
+                });
+            },
+            getBodySize = function() {
+                var w = window,
+                    d = document,
+                    e = d.documentElement,
+                    g = d.body,
+                    x = w.innerWidth || e.clientWidth || g.clientWidth,
+                    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+                return {'x': x, 'y': y};
+            };
 
-    nanoajax.ajax({url:'/hit'}, function (code, responseText) {
-        hit = JSON.parse(responseText).id;
-    });
+        getNextHit();
 
-    return {
-        nextHit: function() {return hit;}
-    };
-})();
-
-var tag = document.createElement('script');
+        return {
+            getBodySize: getBodySize,
+            nextHit: function() {getNextHit(); return hit;}
+        };
+    })(),
+    tag = document.createElement('script'),
+    firstScriptTag = document.getElementsByTagName('script')[0],
+    player;
 
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
 function onYouTubeIframeAPIReady() {
 
     if (!bbcParty.nextHit().length) {
@@ -24,19 +36,29 @@ function onYouTubeIframeAPIReady() {
         return;
     }
 
+    bodySize = bbcParty.getBodySize();
     player = new YT.Player('player', {
-        height: '390',
-        width: '640',
+        height: bodySize.y - 20,
+        width: bodySize.x - 20,
         events: {
-            'onReady': onPlayerReady
+            'onReady': onPlayerReady,
+            'onStateChange': onStateChange
         }
     });
-
-    console.log("data", player);
 }
 
-// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
     player.loadVideoById(bbcParty.nextHit());
     event.target.playVideo();
 }
+
+function onStateChange(event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        player.loadVideoById(bbcParty.nextHit());
+    }
+}
+
+window.onresize = function(event) {
+    bodySize = bbcParty.getBodySize();
+    player.setSize(bodySize.x - 20 , bodySize.y - 20);
+};
